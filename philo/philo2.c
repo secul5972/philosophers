6bbcd89178b454a7ccf2e6e5122627a7acf2e29d
secul5972 <secul5972@naver.com>
2022-02-03 14:16:34 +0900

    before_eval3

--- philo/philo2.c
+++ philo/philo2.c
@@ -6,7 +6,7 @@
 /*   By: seungcoh <seungcoh@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 16:12:01 by seungcoh          #+#    #+#             */
-/*   Updated: 2022/02/03 13:54:40 by seungcoh         ###   ########.fr       */
+/*   Updated: 2022/02/03 14:08:31 by seungcoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,25 @@
 int	p_create(pthread_t **philo, t_p_data *p_data, int *esc_flag)
 {
 	int		i;
-	long	start_t;
 
 	*philo = (pthread_t *)malloc(sizeof(pthread_t) * p_data->n);
 	if (!*philo)
 		return (0);
-	start_t = get_time(0);
+	p_data->start_t = get_time(0);
 	i = -1;
 	while (++i < p_data->n / 2)
 	{
-		(p_data + i * 2 + 1)->start_t = start_t;
+		(p_data + i * 2 + 1)->start_t = p_data->start_t;
 		(p_data + i * 2 + 1)->esc_flag = esc_flag;
 		if (pthread_create((*philo + i * 2 + 1), \
 		0, func, (void *)(p_data + i * 2 + 1)))
 			return (0);
 	}
-	usleep(50);
+	usleep(5 * p_data->n);
 	i = -1;
 	while (++i < p_data->n / 2 + p_data->n % 2)
 	{
-		(p_data + i * 2)-> start_t = start_t;
+		(p_data + i * 2)-> start_t = p_data->start_t;
 		(p_data + i * 2)->esc_flag = esc_flag;
 		if (pthread_create((*philo + i * 2), 0, func, (void *)(p_data + i * 2)))
 			return (0);
@@ -49,6 +48,7 @@ void	p_esc(pthread_t *philo, t_p_data *p_data, int *esc_flag)
 
 	while (!(*esc_flag == -1 || *esc_flag == p_data->n))
 		;
+	usleep(5 * p_data->n);
 	i = -1;
 	while (++i < p_data->n)
 		pthread_join(philo[i], (void *)&ret);
